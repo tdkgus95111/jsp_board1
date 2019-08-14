2f02bb7313c933a63d75cea407d6f5b732265b68
LSH <tdkgus95111@naver.com>
2019-08-14 14:43:13 +0900

    Jsp, Oracle 을 활용한 게시판

---
+++ WebContent/boardUpdateError.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>게시판 글 수정 에러</title>
+</head>
+<body>
+	<h3>비밀 번호가 맞지 않아 수정할 수 없습니다.</h3>
+	<a href="boardList.bbs">[목록으로]</a>
+</body>
+</html>
---
+++ src/board/command/BoardDeleteCmd.java
@@ -0,0 +1,14 @@
+package board.command;
+
+import javax.servlet.http.*;
+
+import board.model.*;
+
+public class BoardDeleteCmd implements BoardCmd{
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		String inputNum = request.getParameter("num");
+		
+		BoardDAO dao = new BoardDAO();
+		dao.boardDelete(inputNum);
+	}
+}
